5bfce21e1a076afebcd7655624ff19f39f504be2
peacejung <peacejung@douzone.com>
2019-08-03 17:45:11 +0900

    test log

--- postgre/index.js
+++ postgre/index.js
@@ -1,25 +1,25 @@
 const { Client } = require('pg');
-const {
-  postgresql: { user, password, host, port, database }
-} = require('../config.json');
+// const {
+//   postgresql: { user, password, host, port, database }
+// } = require('../config.json');
 
 // const connectionString = `tcp://${user}:${password}@${host}:${port}/${database}`;
 console.log(process.env);
 console.log(process.env.DATABASE_URL);
-const client = new Client({
-  user,
-  host,
-  database,
-  password,
-  port
-});
+// const client = new Client({
+//   user,
+//   host,
+//   database,
+//   password,
+//   port
+// });
 
-client.connect((err, client) => {
-  console.log('err: ', err);
-  console.log('client: ', client);
-});
+// client.connect((err, client) => {
+//   console.log('err: ', err);
+//   console.log('client: ', client);
+// });
 
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-  client.end();
-});
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res);
+//   client.end();
+// });
