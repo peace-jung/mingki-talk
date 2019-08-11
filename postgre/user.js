a134acbba2341730a9b785af765b4e843438a586
peace-jung <hopuo132@gmail.com>
2019-08-11 15:21:44 +0900

    add user select

--- postgre/user.js
+++ postgre/user.js
@@ -43,6 +43,7 @@ module.exports = client => {
         } catch (err) {
           return { error: err, code: err.code || 400 };
         }
+
       // 특정 유저 정보 조회
       case 'USERINFO':
         const { userId, password } = data;
@@ -56,18 +57,32 @@ module.exports = client => {
         try {
           const result = await client.query(query);
           if (result.rows.length === 0)
-            return { error: err, code: err.code || 404 };
+            return { error: 'Not Found', code: 404 };
           return { result: 'success', data: result.rows };
         } catch (err) {
           return { error: err, code: err.code || 400 };
         }
+
       // 유저 검색
       case 'SEARCH':
+        const { type, value } = data;
+        if (type !== 'id' && type !== 'phone') {
+          return { error: 'Check Parameter', code: 400 };
+        }
         query = {
           name: 'user-search',
-          text: 'SELECT * FROM public."user" WHERE id = $1',
-          values: [userId]
+          text: `SELECT id, name, phone, profile_img, title, birthday FROM public."user" WHERE ${type} = $1`,
+          values: [value]
         };
+
+        try {
+          const result = await client.query(query);
+          if (result.rows.length === 0)
+            return { error: 'Not Found', code: 404 };
+          return { result: 'success', data: result.rows };
+        } catch (err) {
+          return { error: 'Check Parameter', code: err.code || 400 };
+        }
       default:
         return { error: 'Check Service', code: 400 };
     }
